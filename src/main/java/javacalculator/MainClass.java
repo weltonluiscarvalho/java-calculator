118be0dca2a7a9f5c192164bd725e33a46846de8
Welton Carvalho <welton.carvalho.dev@gmail.com>
2023-06-30 01:12:22 -0300

    create the initial blank screen

---
+++ src/main/java/javacalculator/MainClass.java
@@ -0,0 +1,17 @@
+package javacalculator;
+
+import javax.swing.JFrame;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		JFrame frame = new JFrame();
+		frame.setTitle("Java Calculator");
+		frame.setSize(500, 500);
+		frame.setLocationRelativeTo(null);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
+
+	}
+
+}
