7f2572d8bc5ceeb10a8db4a1ad1bea60574545a3
Welton Carvalho <welton.carvalho.dev@gmail.com>
2023-07-01 00:40:24 -0300

    add basics components to the screen
    without positioning them

--- src/main/java/javacalculator/MainClass.java
+++ src/main/java/javacalculator/MainClass.java
@@ -1,11 +1,59 @@
 package javacalculator;
 
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.Graphics;
+import java.awt.Insets;
+
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.border.Border;
 
 public class MainClass {
 
 	public static void main(String[] args) {
+		
+		JLabel label = new JLabel("visor");
+		Border visorBorder = BorderFactory.createLineBorder(Color.BLACK, 2);
+		label.setBorder(visorBorder);
+		
+		JButton button1 = new JButton("1");
+		JButton button2 = new JButton("2");
+		JButton button3 = new JButton("3");
+		JButton button4 = new JButton("4");
+		JButton button5 = new JButton("5");
+		JButton button6 = new JButton("6");
+		JButton button7 = new JButton("7");
+		JButton button8 = new JButton("8");
+		JButton button9 = new JButton("9");
+		JButton button0 = new JButton("0");
+		JButton buttonPlus = new JButton("+");
+		JButton buttonMinus = new JButton("-");
+		JButton buttonEqual = new JButton("=");
+		
+		JPanel panel = new JPanel();
+		
 		JFrame frame = new JFrame();
+		panel.add(label);
+		panel.add(button1);
+		panel.add(button2);
+		panel.add(button3);
+		panel.add(button4);
+		panel.add(button5);
+		panel.add(button6);
+		panel.add(button7);
+		panel.add(button8);
+		panel.add(button9);
+		panel.add(button0);
+		panel.add(buttonPlus);
+		panel.add(buttonMinus);
+		panel.add(buttonEqual);
+		
+		frame.add(panel);
+		
 		frame.setTitle("Java Calculator");
 		frame.setSize(500, 500);
 		frame.setLocationRelativeTo(null);
